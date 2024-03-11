bfa7e44b08fc48db26c4375e4989b9f69d00c291
nguyễn thái nhân <ntnhan90@gmail.com>
2024-03-11 23:26:03 +0700

    update layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,11 @@
 import { Container } from "react-bootstrap";
 import React from "react";
+import SidebarProvider from "../ui/dashboard/sidebar-provider";
+import SidebarOverlay from "../ui/dashboard/Sidebar/SidebarOverlay";
+import Sidebar from "../ui/dashboard/Sidebar/Sidebar";
+import SidebarNav from "../ui/dashboard/Sidebar/SidebarNav";
+import Header from "../ui/dashboard/Header/Header";
+import Footer from "../ui/dashboard/Footer/Footer";
 
 export default function Layout({
     children,
@@ -7,16 +13,20 @@ export default function Layout({
     children:React.ReactNode
 }) {
     return(
-        <>
+        <SidebarProvider>
+            <SidebarOverlay />
+            <Sidebar>
+                <SidebarNav />
+            </Sidebar>
             <div className="wrapper d-flex flex-column min-vh-100 bg-light">
-
+                <Header />
                 <div className="body flex-grow-1 px-sm-2 mb-4">
                     <Container fluid="lg">
                         {children}
                     </Container>
                 </div>
-
+                <Footer />
             </div>
-        </>
+        </SidebarProvider>
     )
 }
